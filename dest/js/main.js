f1a5168fef92b1e15c9efa5c6fe58f61353d770b
Shotaro Kohama <khmshtr28@gmail.com>
2013-12-22 12:59:17 +0900

    Add images and sounds.

--- dest/js/main.js
+++ dest/js/main.js
@@ -1,6 +1,7 @@
-var Main, arraysEqual, clearGuide, is_affected, judge, pickQueenImageNumber, refreshGuide, removeElem, replaceScene, reset, showMessage, showResult, startTimer, switchGuide, toggle, updateCountLabel, updateJudgeButtonState, updateTimeLabel;
+var Main, are_three_in_a_line, arraysEqual, clearConflict, clearGuide, judge, pickQueenImageNumber, playBGM, playClick, playNyah, playSound, refreshGuide, removeElem, replaceScene, reset, showMessage, showResult, startTimer, switchGuide, toggle, updateCountLabel, updateJudgeButtonState, updateTimeLabel;
 
 Main = {
+  max_piece: 16,
   blocks: [[], [], [], [], [], [], [], []],
   queens: [],
   guides: [],
@@ -9,53 +10,8 @@ Main = {
   time: 0,
   timer: null,
   result: false,
-  queenImageUsed: [false, false, false, false, false, false, false, false]
+  queenImageUsed: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]
 };
-
-$(function() {
-  var block, blocks, i, j, _i, _j;
-  replaceScene('intro');
-  blocks = $('.block');
-  for (i = _i = 0; _i < 8; i = ++_i) {
-    for (j = _j = 0; _j < 8; j = ++_j) {
-      block = blocks[i * 8 + j];
-      block.x = i;
-      block.y = j;
-      block.status = 0;
-      block.onclick = function() {
-        return toggle(this);
-      };
-      Main.blocks[i].push(block);
-    }
-  }
-  updateCountLabel();
-  $('#startbutton').click(function() {
-    replaceScene('main');
-    Main.time = 0;
-    startTimer();
-    return updateTimeLabel();
-  });
-  $('#judgebutton').click(function() {
-    if (!$('#judgebutton').hasClass('disabled')) {
-      return showResult();
-    }
-  });
-  $('#resetbutton').click(function() {
-    return reset();
-  });
-  $('#backbutton').click(function() {
-    if (Main.result) {
-      reset();
-      Main.guideEnabled = true;
-      switchGuide();
-      replaceScene('intro');
-    }
-    return $('#message').hide();
-  });
-  return $('#guidebutton').click(function() {
-    return switchGuide();
-  });
-});
 
 replaceScene = function(id) {
   var i, s, scenes, _i, _len;
@@ -71,23 +27,23 @@ toggle = function(block) {
   var imageNum, queen;
   if (block.status) {
     $(block).removeClass('active');
-    console.log('cat' + block.image);
+    console.log('Remove cat' + block.image);
     $(block).removeClass('cat' + block.image);
     Main.queenImageUsed[block.image] = false;
     block.status = 0;
     Main.count--;
-    console.log("hogehoge");
     queen = {
       x: block.x,
       y: block.y
     };
     console.log(Main.queens);
     removeElem(Main.queens, queen);
-    console.log("fugafuga");
-  } else if (Main.count < 5) {
+  } else if (Main.count < Main.max_piece) {
     imageNum = pickQueenImageNumber();
+    Main.queenImageUsed[imageNum] = true;
     $(block).addClass('active');
     $(block).addClass('cat' + imageNum);
+    console.log('Add cat' + imageNum);
     block.status = 1;
     block.image = imageNum;
     Main.count++;
@@ -95,19 +51,19 @@ toggle = function(block) {
       x: block.x,
       y: block.y
     };
+    console.log(block.x + "," + block.y);
     Main.queens.push(queen);
-    console.log("hoge");
+    playNyah();
   }
   if (Main.guideEnabled) {
     refreshGuide();
   }
-  console.log("fuga");
   updateCountLabel();
   return updateJudgeButtonState();
 };
 
 updateJudgeButtonState = function() {
-  if (Main.count === 5) {
+  if (Main.count === Main.max_piece) {
     return $('#judgebutton').removeClass('disabled');
   } else {
     return $('#judgebutton').addClass('disabled', 'disabled');
@@ -115,41 +71,45 @@ updateJudgeButtonState = function() {
 };
 
 judge = function() {
-  var i, is_ok, j, q, _i, _j, _k, _len, _ref;
-  for (i = _i = 0; _i < 8; i = ++_i) {
-    for (j = _j = 0; _j < 8; j = ++_j) {
-      is_ok = false;
-      _ref = Main.queens;
-      for (_k = 0, _len = _ref.length; _k < _len; _k++) {
-        q = _ref[_k];
-        if (is_affected(i, j, q)) {
-          is_ok = true;
-          continue;
+  var i, j, k, _i, _j, _k, _ref, _ref1, _ref2;
+  for (i = _i = 2, _ref = Main.max_piece; 2 <= _ref ? _i < _ref : _i > _ref; i = 2 <= _ref ? ++_i : --_i) {
+    for (j = _j = 1, _ref1 = i - 1; 1 <= _ref1 ? _j < _ref1 : _j > _ref1; j = 1 <= _ref1 ? ++_j : --_j) {
+      for (k = _k = 0, _ref2 = j - 1; 0 <= _ref2 ? _k <= _ref2 : _k >= _ref2; k = 0 <= _ref2 ? ++_k : --_k) {
+        if (are_three_in_a_line(i, j, k)) {
+          return false;
         }
-      }
-      if (!is_ok) {
-        return false;
       }
     }
   }
   return true;
 };
 
-is_affected = function(x, y, queen) {
-  var diagonal, horizon, nx, ny, qx, qy, vertical;
-  nx = x + 1;
-  ny = y + 1;
-  qx = queen.x + 1;
-  qy = queen.y + 1;
-  horizon = qx === nx;
-  vertical = qy === ny;
-  diagonal = (ny + nx === qx + qy) || (nx - ny === qx - qy);
-  return horizon || vertical || diagonal;
+are_three_in_a_line = function(i, j, k) {
+  var dx12, dx13, dy12, dy13, q1, q2, q3, _ref, _ref1, _ref2;
+  if (i === j || j === k || i === k) {
+    return false;
+  }
+  q1 = Main.queens[i];
+  q2 = Main.queens[j];
+  q3 = Main.queens[k];
+  if (q1.x > q2.x) {
+    _ref = [q2, q1], q1 = _ref[0], q2 = _ref[1];
+  }
+  if (q1.x > q3.x) {
+    _ref1 = [q3, q1], q1 = _ref1[0], q3 = _ref1[1];
+  }
+  if (q2.x > q3.x) {
+    _ref2 = [q3, q2], q2 = _ref2[0], q3 = _ref2[1];
+  }
+  dx12 = q2.x - q1.x;
+  dy12 = q2.y - q1.y;
+  dx13 = q3.x - q1.x;
+  dy13 = q3.y - q1.y;
+  return dy12 * dx13 === dx12 * dy13;
 };
 
 updateCountLabel = function() {
-  console.log("call updateCountLabel!!! " + Main.count);
-  return $('#countlabel').html('×' + Main.count + '/5');
+  return $('#countlabel').html('×' + Main.count + '/' + Main.max_piece);
 };
 
 updateTimeLabel = function() {
@@ -174,6 +134,7 @@ reset = function() {
     for (j = _j = 0; _j < 8; j = ++_j) {
       block = Main.blocks[i][j];
       block.status = 0;
+      block.conflict = false;
       $(block).removeClass('active');
       $(block).removeClass('mark');
       $(block).removeClass(function(index, css) {
@@ -223,59 +184,39 @@ switchGuide = function() {
 };
 
 refreshGuide = function() {
-  var block, guide, i, q, queens, x, y, _i, _len, _results;
+  var block, guide, i, j, k, mark, q, qb, qi, qj, qk, queens, _i, _j, _k, _l, _len, _ref, _ref1, _results;
+  console.log("call refreshGuide!!!");
   clearGuide();
+  clearConflict();
   queens = Main.queens;
-  _results = [];
-  for (i = _i = 0, _len = queens.length; _i < _len; i = ++_i) {
-    q = queens[i];
-    _results.push((function() {
-      var _j, _results1;
-      _results1 = [];
-      for (x = _j = 0; _j < 8; x = ++_j) {
-        if (x !== q.y) {
-          guide = $('<div>').addClass('guide');
-          guide.x = q.x;
-          guide.y = x;
-          block = $(Main.blocks[q.x][x]);
-          block.append(guide);
-          Main.guides.push(guide);
-        }
-        if (x !== q.x) {
-          guide = $('<div>').addClass('guide');
-          guide.x = x;
-          guide.y = q.y;
-          block = $(Main.blocks[x][q.y]);
-          block.append(guide);
-          Main.guides.push(guide);
-        }
-        if (x !== q.x) {
-          y = -x + q.x + q.y;
-          if ((0 <= y && y < 8)) {
-            guide = $('<div>').addClass('guide');
-            guide.x = x;
-            guide.y = y;
-            block = $(Main.blocks[x][y]);
-            block.append(guide);
-            Main.guides.push(guide);
-          }
-          y = x + q.y - q.x;
-          if ((0 <= y && y < 8)) {
-            guide = $('<div>').addClass('guide');
-            guide.x = x;
-            guide.y = y;
-            block = $(Main.blocks[x][y]);
-            block.append(guide);
-            _results1.push(Main.guides.push(guide));
-          } else {
-            _results1.push(void 0);
-          }
-        } else {
-          _results1.push(void 0);
+  for (i = _i = 0, _ref = queens.length; 0 <= _ref ? _i < _ref : _i > _ref; i = 0 <= _ref ? ++_i : --_i) {
+    for (j = _j = 0; 0 <= i ? _j < i : _j > i; j = 0 <= i ? ++_j : --_j) {
+      for (k = _k = 0; 0 <= j ? _k < j : _k > j; k = 0 <= j ? ++_k : --_k) {
+        console.log(i + "," + j + "," + k);
+        if (are_three_in_a_line(i, j, k)) {
+          console.log("    conflict!!!");
+          _ref1 = [queens[i], queens[j], queens[k]], qi = _ref1[0], qj = _ref1[1], qk = _ref1[2];
+          Main.blocks[qi.x][qi.y].conflict = true;
+          Main.blocks[qj.x][qj.y].conflict = true;
+          Main.blocks[qk.x][qk.y].conflict = true;
         }
       }
-      return _results1;
-    })());
+    }
+  }
+  _results = [];
+  for (i = _l = 0, _len = queens.length; _l < _len; i = ++_l) {
+    q = queens[i];
+    guide = $('<div>').addClass('guide');
+    guide.x = q.x;
+    guide.y = q.y;
+    qb = Main.blocks[q.x][q.y];
+    block = $(qb);
+    block.append(guide);
+    if (qb.conflict) {
+      mark = $('<div>').addClass('mark');
+      $(guide).append(mark);
+    }
+    _results.push(Main.guides.push(guide));
   }
   return _results;
 };
@@ -291,16 +232,54 @@ clearGuide = function() {
   return Main.guides = [];
 };
 
+clearConflict = function() {
+  var i, j, _i, _results;
+  _results = [];
+  for (i = _i = 0; _i < 8; i = ++_i) {
+    _results.push((function() {
+      var _j, _results1;
+      _results1 = [];
+      for (j = _j = 0; _j < 8; j = ++_j) {
+        _results1.push(Main.blocks[i][j].conflict = false);
+      }
+      return _results1;
+    })());
+  }
+  return _results;
+};
+
 pickQueenImageNumber = function() {
   var b, i, _i, _len, _ref;
   _ref = Main.queenImageUsed;
   for (i = _i = 0, _len = _ref.length; _i < _len; i = ++_i) {
     b = _ref[i];
     if (!b) {
-      Main.queenImageUsed[i] = true;
       return i;
     }
   }
+};
+
+playBGM = function() {
+  var bgm;
+  bgm = new Audio('../sound/bgm.wav');
+  bgm.loop = true;
+  return bgm.play();
+};
+
+playNyah = function() {
+  Main.nyahSound = new Audio('../sound/cat.wav');
+  return Main.nyahSound.play();
+};
+
+playClick = function() {
+  Main.clickSound = new Audio('../sound/click.wav');
+  return Main.clickSound.play();
+};
+
+playSound = function(path) {
+  var sound;
+  sound = new Audio(path);
+  return sound.play();
 };
 
 removeElem = function(array, value) {
@@ -345,3 +324,55 @@ arraysEqual = function(arrayA, arrayB) {
   }
   return true;
 };
+
+$(function() {
+  var block, blocks, i, j, _i, _j;
+  playBGM();
+  replaceScene('intro');
+  blocks = $('.block');
+  for (i = _i = 0; _i < 8; i = ++_i) {
+    for (j = _j = 0; _j < 8; j = ++_j) {
+      block = blocks[i * 8 + j];
+      block.x = i;
+      block.y = j;
+      block.status = 0;
+      block.conflict = false;
+      console.log(i + "," + j);
+      block.onclick = function() {
+        return toggle(this);
+      };
+      Main.blocks[i].push(block);
+    }
+  }
+  updateCountLabel();
+  $('#startbutton').click(function() {
+    replaceScene('main');
+    Main.time = 0;
+    startTimer();
+    updateTimeLabel();
+    return playSound('../sound/start.wav');
+  });
+  $('#judgebutton').click(function() {
+    playClick();
+    if (!$('#judgebutton').hasClass('disabled')) {
+      return showResult();
+    }
+  });
+  $('#resetbutton').click(function() {
+    playSound('../sound/reset.wav');
+    return reset();
+  });
+  $('#backbutton').click(function() {
+    if (Main.result) {
+      reset();
+      Main.guideEnabled = true;
+      switchGuide();
+      replaceScene('intro');
+      playClick();
+    }
+    return $('#message').hide();
+  });
+  return $('#guidebutton').click(function() {
+    return switchGuide();
+  });
+});
--- src/coffee/main.coffee
+++ src/coffee/main.coffee
@@ -1,4 +1,5 @@
 Main =
+  max_piece: 16
   blocks: [[],[],[],[],[],[],[],[]]
   queens: []
   guides: []
@@ -7,44 +8,8 @@ Main =
   time: 0
   timer: null
   result: false
-  queenImageUsed: [false, false, false, false, false, false, false, false]
-
-$ ->
-  replaceScene('intro')
-  #ブロック要素を二次元配列に格納
-  blocks = $('.block')
-  for i in [0...8]
-    for j in [0...8]
-      block = blocks[i*8+j]
-      block.x = i
-      block.y = j
-      block.status = 0
-      block.onclick = ->
-        toggle(this)
-      Main.blocks[i].push(block)
-  updateCountLabel()
-  $('#startbutton').click ->
-    replaceScene('main')
-    Main.time = 0
-    startTimer()
-    updateTimeLabel()
-  $('#judgebutton').click ->
-    if !$('#judgebutton').hasClass('disabled')
-      showResult()
-
-  $('#resetbutton').click ->
-    reset()
-
-  $('#backbutton').click ->
-    if Main.result
-      reset()
-      Main.guideEnabled = true
-      switchGuide()
-      replaceScene('intro')
-    $('#message').hide()
-
-  $('#guidebutton').click ->
-    switchGuide()
+  queenImageUsed: [false, false, false, false, false, false, false, false,
+                   false, false, false, false, false, false, false, false]
 
 replaceScene = (id) ->
   scenes = ['intro', 'main', 'message']
@@ -55,64 +20,71 @@ replaceScene = (id) ->
 toggle = (block) ->
   if block.status
     $(block).removeClass('active')
-    console.log 'cat'+block.image
-    $(block).removeClass('cat'+block.image)
+    console.log 'Remove cat'+ block.image
+    $(block).removeClass('cat' + block.image)
     Main.queenImageUsed[block.image] = false
     block.status = 0
     Main.count--
-    console.log "hogehoge"
     queen = {x: block.x, y: block.y}
     console.log Main.queens
     removeElem(Main.queens, queen)
-    console.log "fugafuga"
-  else if Main.count < 5
+  else if Main.count < Main.max_piece
     imageNum = pickQueenImageNumber()
+    Main.queenImageUsed[imageNum] = true
     $(block).addClass('active')
-    $(block).addClass('cat'+imageNum)
+    $(block).addClass('cat' + imageNum)
+    console.log 'Add cat' + imageNum
     block.status = 1
     block.image = imageNum
     Main.count++
-
     queen = {x: block.x, y: block.y}
+    console.log block.x + "," + block.y
     Main.queens.push(queen)
-    console.log "hoge"
+    playNyah()
   if Main.guideEnabled
     refreshGuide()
-  console.log "fuga"
   updateCountLabel()
   updateJudgeButtonState()
 
 updateJudgeButtonState = ->
-  if Main.count==5
+  if Main.count == Main.max_piece
     $('#judgebutton').removeClass('disabled')
   else
     $('#judgebutton').addClass('disabled', 'disabled')
 
 judge = ->
-  for i in [0...8]
-    for j in [0...8]
-      is_ok = false
-      for q in Main.queens
-        if is_affected(i, j, q)
-          is_ok = true
-          continue
-      if !is_ok
-        return false
+  for i in [2...Main.max_piece]
+    for j in [1...(i-1)]
+      for k in [0..(j-1)]
+        if are_three_in_a_line(i, j, k)
+          return false
   return true
 
-is_affected = (x, y, queen) ->
-  nx = x + 1
-  ny = y + 1
-  qx = queen.x + 1
-  qy = queen.y + 1
-  horizon = (qx == nx)
-  vertical = (qy == ny)
-  diagonal = (ny + nx == qx + qy) || (nx - ny == qx - qy)
-  return (horizon or vertical or diagonal)
+are_three_in_a_line = (i, j, k) ->
+  if i == j || j == k || i == k
+    return false
+  q1 = Main.queens[i]
+  q2 = Main.queens[j]
+  q3 = Main.queens[k]
+  # q1.x < q2.x
+  if q1.x > q2.x
+    [q1, q2] = [q2, q1]
+  # q1.x < q3.x
+  if q1.x > q3.x
+    [q1, q3] = [q3, q1]
+  # q2.x < q3.x
+  if q2.x > q3.x
+    [q2, q3] = [q3, q2]
+
+  dx12 = q2.x - q1.x
+  dy12 = q2.y - q1.y
+  dx13 = q3.x - q1.x
+  dy13 = q3.y - q1.y
+
+  return (dy12 * dx13 == dx12 * dy13)
 
 updateCountLabel = ->
-  console.log "call updateCountLabel!!! " + Main.count
-  $('#countlabel').html('×' + Main.count + '/5')
+  $('#countlabel').html('×' + Main.count + '/' + Main.max_piece)
 
 updateTimeLabel = ->
   $('#timelabel').html(Main.time + '秒')
@@ -132,6 +104,7 @@ reset = ->
     for j in [0...8]
       block = Main.blocks[i][j]
       block.status = 0
+      block.conflict = false
       $(block).removeClass('active')
       $(block).removeClass('mark')
       $(block).removeClass (index, css) ->
@@ -169,56 +142,65 @@ switchGuide = ->
   Main.guideEnabled = !Main.guideEnabled
 
 refreshGuide = ->
+  console.log "call refreshGuide!!!"
   clearGuide()
+  clearConflict()
   queens = Main.queens
+  for i in [0...queens.length]
+    for j in [0...i]
+      for k in [0...j]
+        console.log i + "," + j + "," + k
+        if are_three_in_a_line(i, j, k)
+          console.log "    conflict!!!"
+          [qi, qj, qk] = [queens[i], queens[j], queens[k]]
+          Main.blocks[qi.x][qi.y].conflict = true
+          Main.blocks[qj.x][qj.y].conflict = true
+          Main.blocks[qk.x][qk.y].conflict = true
+
   for q, i in queens
-    for x in [0...8]
-      #縦横
-      if x!=q.y
-        guide = $('<div>').addClass('guide')
-        guide.x = q.x
-        guide.y = x
-        block = $(Main.blocks[q.x][x])
-        block.append(guide)
-        Main.guides.push(guide)
-      if x!=q.x
-        guide = $('<div>').addClass('guide')
-        guide.x = x
-        guide.y = q.y
-        block = $(Main.blocks[x][q.y])
-        block.append(guide)
-        Main.guides.push(guide)
-      #斜め
-      if x!=q.x
-        y = -x + q.x+q.y
-        if 0<=y<8
-          guide = $('<div>').addClass('guide')
-          guide.x = x
-          guide.y = y
-          block = $(Main.blocks[x][y])
-          block.append(guide)
-          Main.guides.push(guide)
-
-        y = x + q.y-q.x
-        if 0<=y<8
-          guide = $('<div>').addClass('guide')
-          guide.x = x
-          guide.y = y
-          block = $(Main.blocks[x][y])
-          block.append(guide)
-          Main.guides.push(guide)
-
+    guide = $('<div>').addClass('guide')
+    guide.x = q.x
+    guide.y = q.y
+    qb = Main.blocks[q.x][q.y]
+    block = $(qb)
+    block.append(guide)
+    if qb.conflict
+      mark = $('<div>').addClass('mark')
+      $(guide).append(mark)
+    Main.guides.push(guide)
+    
 clearGuide = ->
   for g, i in Main.guides
     $(Main.blocks[g.x][g.y]).removeClass('mark')
     g.remove()
   Main.guides = []
 
+clearConflict = ->
+  for i in [0...8]
+    for j in [0...8]
+      Main.blocks[i][j].conflict = false
+
 pickQueenImageNumber = ->
   for b, i in Main.queenImageUsed
     if !b
-      Main.queenImageUsed[i] = true
       return i
+
+playBGM = ->
+  bgm = new Audio('../sound/bgm.wav')
+  bgm.loop = true
+  bgm.play()
+
+playNyah = ->
+  Main.nyahSound = new Audio('../sound/cat.wav')
+  Main.nyahSound.play();
+
+playClick = ->
+  Main.clickSound = new Audio('../sound/click.wav')
+  Main.clickSound.play();
+
+playSound = (path) ->
+  sound = new Audio(path)
+  sound.play()
 
 #Foundation
 removeElem = (array, value) ->
@@ -245,5 +227,50 @@ arraysEqual = (arrayA, arrayB) ->
       if a!=b
         return false 
   return true
+
+$ ->
+  playBGM()
+  replaceScene('intro')
+  #ブロック要素を二次元配列に格納
+  blocks = $('.block')
+  for i in [0...8]
+    for j in [0...8]
+      block = blocks[i*8+j]
+      block.x = i
+      block.y = j
+      block.status = 0
+      block.conflict = false
+      console.log i + "," + j
+      block.onclick = ->
+        toggle(this)
+      Main.blocks[i].push(block)
+  updateCountLabel()
+  $('#startbutton').click ->
+    replaceScene('main')
+    Main.time = 0
+    startTimer()
+    updateTimeLabel()
+    playSound('../sound/start.wav')    
+
+  $('#judgebutton').click ->
+    playClick()
+    if !$('#judgebutton').hasClass('disabled')
+      showResult()
+
+  $('#resetbutton').click ->
+    playSound('../sound/reset.wav')
+    reset()
+
+  $('#backbutton').click ->
+    if Main.result
+      reset()
+      Main.guideEnabled = true
+      switchGuide()
+      replaceScene('intro')
+      playClick()      
+    $('#message').hide()
+
+  $('#guidebutton').click ->
+    switchGuide()
 
 
